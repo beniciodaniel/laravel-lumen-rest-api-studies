958dc0a582c10dd99fb30bdb99e29686c2d3afc1
Benicio <benicio.daniel@gmail.com>
2019-11-09 14:17:57 -0300

    Este commit implementou o método update utilizando o PUT noo Postman

--- api-series/app/Http/Controllers/SeriesController.php
+++ api-series/app/Http/Controllers/SeriesController.php
@@ -17,6 +17,29 @@ class SeriesController
     public function store(Request $request)
     {
         return response()
-            ->json(Serie::create(['nome' => $request->nome]), 201);
+            ->json(Serie::create($request->all()), 201);
+    }
+
+    public function show(int $id)
+    {
+        $serie = Serie::find($id);
+        if (is_null($serie)) {
+            return response()->json('', 204);
+        }
+        return response()->json($serie);
+    }
+
+    public function update(int $id, Request $request)
+    {
+        $serie = Serie::find($id);
+        if (is_null($serie)) {
+            return response()->json([
+                'erro' => 'Recurso não encontrado'
+            ], 404);
+        }
+        $serie->fill($request->all());
+        $serie->save();
+
+        return $serie;
     }
 }
