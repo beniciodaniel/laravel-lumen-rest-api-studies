463e87e2f6404bb359670f09b2b3d01d145f328b
Benicio <benicio.daniel@gmail.com>
2019-11-10 11:17:19 -0300

    Este commit implementou a refatoração dos controllers. Criou-se um Controller base para fornecer funções e métodos para as subclasses

---
+++ api-series/app/Http/Controllers/BaseController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use Illuminate\Http\Request;
+
+abstract class BaseController
+{
+    protected $classe;
+
+    public function index()
+    {
+        return $this->classe::all();
+    }
+
+    public function store(Request $request)
+    {
+        return response()
+            ->json($this->classe::create($request->all()), 201); //201 = criado
+    }
+
+    public function show(int $id)
+    {
+        $recurso = $this->classe::find($id);
+        if (is_null($serie)) {
+            return response()->json('', 204); //recurso não encontrado
+        }
+        return response()->json($recurso);
+    }
+
+    public function update(int $id, Request $request)
+    {
+        $recurso = $this->classe::find($id);
+        if (is_null($serie)) {
+            return response()->json([
+                'erro' => 'Recurso não encontrado'
+            ], 404);
+        }
+        $recurso->fill($request->all());
+        $recurso->save();
+
+        return $recurso;
+    }
+
+    public function destroy(int $id)
+    {
+        $qtdadeRecursosRemovidos = $this->classe::destroy($id);
+        if ($qtdadeRecursosRemovidos === 0) {
+            return response()->json([
+                'erro' => 'Recurso não encontrado'
+            ], 404);
+        }
+        return response()->json('', 204);
+    }
+}
